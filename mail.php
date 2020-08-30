394831235ddefc7f8e8056cd1a9c823893387ac5
Max <mitrohmax@yandex.ru>
2020-08-30 11:37:56 +0300

    18

--- mail.php
+++ mail.php
@@ -8,15 +8,15 @@ $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
 
 $mail->isSMTP();
-$mail->Host = 'smtp.mail.ru';
+$mail->Host = 'mail.hosting.reg.ru';
 $mail->SMTPAuth = true;
-$mail->Username = 'maxmitrokhin@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '23357Zima'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'robot@remwoodwind.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'qwerty123'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';
 $mail->Port = 465;
 
-$mail->setFrom('maxmitrokhin@mail.ru'); // От кого будет уходить письмо
-$mail->addAddress('mitrohmax@yandex.ru');     // Кому будет уходить письмо
+$mail->setFrom('robot@remwoodwind.ru'); // От кого будет уходить письмо
+$mail->addAddress('moskitnet@mail.ru');     // Кому будет уходить письмо
 $mail->isHTML(true);
 
 $mail->Subject = 'Заявка с сайта';
